ad602c44c1f3f479a749e825a4dde5baf7c1073a
lmjjiayou <fxzltpply@163.com>
2015-07-30 10:30:07 +0800

    changes content

---
+++ main.js
@@ -0,0 +1,10 @@
+/*var counter1=require('./counter');
+var counter2=require('./counter');
+
+console.log(counter1.cnt());
+console.log(counter2.cnt());
+console.log(counter2.cnt());*/
+console.log('main starting');
+var a = require('./a.js');
+var b = require('./b.js');
+console.log('in main,a.done=%j,b.done=%j',a.done,b.done);
