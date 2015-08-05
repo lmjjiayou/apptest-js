019924be8c7b332d0ad2f32ecad564d64fe314fc
lmj102819 <lmj102819@alibaba-inc.com>
2015-08-05 17:14:56 +0800

    代码提交更新

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var counter2=require('./counter');
 console.log(counter1.cnt());
 console.log(counter2.cnt());
 console.log(counter2.cnt());*/
+//////
+//////
+//////main a b
 console.log('main starting');
 var a = require('./a.js');
 var b = require('./b.js');
