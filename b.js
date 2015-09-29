87ae42bc0663ebd77157047b8f63a5eb17737b16
lmj102819 <lmj102819@alibaba-inc.com>
2015-09-29 15:32:53 +0800

    fixed b file

--- b.js
+++ b.js
@@ -4,8 +4,3 @@ var a = require('./a.js');
 console.log('in b, a.done=%j',a.done);
 exports.done=true;
 console.log('b done');
-<<<<<<< HEAD
-console.log('b.js is done');
-=======
-console.log('b done');
->>>>>>> f1412f49ee82dd8305a4e27958e6e3d70b547e8b
